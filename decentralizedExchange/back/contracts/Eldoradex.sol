df66a89332ed18a0a3def221acf2ae6302f9733f
Hugo Nakache <hugo.nakache@epitech.eu>
2021-10-30 13:38:06 +0200

    Implementation of limit orders. Uses quicksort algorithm to optimise time complexity and gas consumption when sorting the order book.

--- decentralizedExchange/back/contracts/Eldoradex.sol
+++ decentralizedExchange/back/contracts/Eldoradex.sol
@@ -3,10 +3,6 @@ pragma solidity >=0.4.21 <0.9.0;
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 contract Eldoradex {
-    enum Side {
-        BUY,
-        SELL
-    }
 
     struct Token {
         bytes32 ticker;
@@ -15,12 +11,29 @@ contract Eldoradex {
 
     struct Order {
         uint id;
-        bytes32 ticker;
+        uint price;
         uint amount;
+        bytes32 ticker;
         uint filled;
         Side side;
         uint date;
     }
+
+    enum Side {
+        BUY,
+        SELL
+    }
+
+    event NewTrade (
+        uint date,
+        uint idTrade,
+        uint idOrder,
+        address indexed trader1,
+        address indexed trader2,
+        bytes32 indexed ticker,
+        uint amount,
+        uint price
+    );
 
     mapping(bytes32 => Token) public tokens;
     mapping(address => mapping(bytes32 => uint)) public traderBalances;
@@ -28,6 +41,7 @@ contract Eldoradex {
     bytes32[] public tokensList;
     address public admin;
     uint nextOrderId;
+    bytes32 constant EldoraDAI = bytes32('EDAI');
     
     constructor() {
         admin = msg.sender;
@@ -54,12 +68,71 @@ contract Eldoradex {
         tokensList.push(ticker);
     }
 
-    function createLimitOrder(Side side, bytes32 ticker, uint amount, uint price) tokenExistOnExchange(ticker) external {
-        
+    /* 
+        Create a LIMIT buy or sell order and push it to the order book.
+    */
+
+    function createLimitOrder(Side side, bytes32 ticker, uint amount, uint price) 
+    tokenExistOnExchange(ticker) 
+    tokenIsNotEldoraDai(ticker) external {
+        if (side == Side.SELL) {
+            require(traderBalances[msg.sender][ticker] >= amount, "Token balance is too low");
+        }
+        else if (side == Side.BUY) {
+            require(traderBalances[msg.sender][EldoraDAI] >= amount * price, "EDAI balance is too low");
+        }
+        Order[] storage orders = orderBook[ticker][uint(side)];
+        orders.push(Order(
+            nextOrderId,
+            price,
+            amount,
+            ticker,
+            0,
+            side,
+            block.timestamp
+        ));
+        sort(orders);
+        nextOrderId++;
     }
+
+    function sort(Order[] storage orders) internal returns(Order[] storage) {
+       quickSort(orders, int(0), int(orders.length - 1));
+       return orders;
+    }
+    
+    function quickSort(Order[] storage arr, int left, int right) internal {
+        int i = left;
+        int j = right;
+        if (i == j) {
+            return;
+        }
+        uint pivot = arr[uint(left + (right - left) / 2)].price;
+        while (i <= j) {
+            while (arr[uint(i)].price < pivot) i++;
+            while (pivot < arr[uint(j)].price) j--;
+            if (i <= j) {
+                (arr[uint(i)].price, arr[uint(j)].price) = (arr[uint(j)].price, arr[uint(i)].price);
+                i++;
+                j--;
+            }
+        }
+        if (left < j)
+            quickSort(arr, left, j);
+        if (i < right)
+            quickSort(arr, i, right);
+    }
+
+    /*
+        Check if the token is tradeable on the exchange.
+     */
 
     modifier tokenExistOnExchange(bytes32 ticker) {
         require(tokens[ticker].tokenAddress != address(0), 'Token must be tradable on EldoraDEX');
+        _;
+    }
+
+    modifier tokenIsNotEldoraDai(bytes32 ticker) {
+        require(ticker != EldoraDAI, 'EDAI is not allowed to be traded');
         _;
     }
 
