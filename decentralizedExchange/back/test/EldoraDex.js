cb94ca52e53185edd6141443cfa46059bed9ee48
Hugo Nakache <hugo.nakache@epitech.eu>
2021-10-31 12:52:28 +0100

    Implementation of MARKET orders test set

--- decentralizedExchange/back/test/EldoraDex.js
+++ decentralizedExchange/back/test/EldoraDex.js
@@ -67,7 +67,7 @@ contract('EldoraDex', (accounts) => {
         await expectRevert(
             dex.depositTokens(
                 web3.utils.toWei('100'),
-                web3.utils.fromAscii('DOEST_NOT_EXIST'),
+                web3.utils.fromAscii('DOES_NOT_EXIST'),
                 { from : user1 }
             ),
             'Token must be tradable on EldoraDEX'
@@ -111,7 +111,7 @@ contract('EldoraDex', (accounts) => {
         );
     });
 
-    it('should create a LIMIT BUY order', async () => {
+    it('should create several LIMIT orders ordered in the right way depending on the side of the book.', async () => {
         let amount = web3.utils.toWei('500');
 
         await dex.depositTokens(amount, EDAI, {from : user1});
@@ -127,49 +127,139 @@ contract('EldoraDex', (accounts) => {
         assert(buyOrdersList[0].amount === web3.utils.toWei('10'));
         assert(sellOrdersList.length === 0);
 
-        // amount = web3.utils.toWei('500');
+        amount = web3.utils.toWei('500');
 
-        // await dex.depositTokens(amount, EDAI, {from : user2});
-        // await dex.createLimitOrder(SIDE.BUY, EBAT, web3.utils.toWei('10'), 31, {from : user2});
+        await dex.depositTokens(amount, EDAI, {from : user2});
+        await dex.createLimitOrder(SIDE.BUY, EBAT, web3.utils.toWei('10'), 31, {from : user2});
 
-        // buyOrdersList = await dex.getOrders(SIDE.BUY, EBAT);
-        // sellOrdersList = await dex.getOrders(SIDE.SELL, EBAT);
-        // assert(buyOrdersList.length === 3);
-        // assert(buyOrdersList[1].trader === user2);
-        // assert(buyOrdersList[0].trader === user1);
+        buyOrdersList = await dex.getOrders(SIDE.BUY, EBAT);
+        sellOrdersList = await dex.getOrders(SIDE.SELL, EBAT);
+        assert(buyOrdersList.length === 2);
+        assert(buyOrdersList[0].trader === user2);
+        assert(buyOrdersList[1].trader === user1);
+        assert(sellOrdersList.length === 0);
 
-        // await dex.createLimitOrder(SIDE.BUY, EBAT, web3.utils.toWei('10'), 29, {from : user2});
+        await dex.createLimitOrder(SIDE.BUY, EBAT, web3.utils.toWei('10'), 29, {from : user2});
 
-        // buyOrdersList = await dex.getOrders(SIDE.BUY, EBAT);
-        // sellOrdersList = await dex.getOrders(SIDE.SELL, EBAT);
-        // console.log(buyOrdersList)
-        // assert(buyOrdersList[0].trader === user2);
-        // assert(buyOrdersList[1].trader === user1);
-        // assert(buyOrdersList[2].trader === user2);
-        // assert(buyOrdersList[0].price === '29');
-        // assert(buyOrdersList[2].price === '31');
+        buyOrdersList = await dex.getOrders(SIDE.BUY, EBAT);
+        sellOrdersList = await dex.getOrders(SIDE.SELL, EBAT);
+        assert(buyOrdersList.length === 3);
+        assert(buyOrdersList[0].trader === user2);
+        assert(buyOrdersList[1].trader === user1);
+        assert(buyOrdersList[2].trader === user2);
+        assert(buyOrdersList[0].price === '31');
+        assert(buyOrdersList[2].price === '29');
+        assert(sellOrdersList.length === 0);
     });
 
-    // it('should create a LIMIT SELL order', async () => {
-    //     const amount = web3.utils.toWei('100');
+    it('should NOT create LIMIT BUY order if the token does not exist', async() => {
+        await expectRevert(
+            dex.createLimitOrder(SIDE.BUY, 
+                web3.utils.fromAscii('DOES_NOT_EXIST'),
+                web3.utils.toWei('10'), 
+                30, 
+                {from : user1}),
+            'Token must be tradable on EldoraDEX'
+        );
+    });
 
-    //     await dex.depositTokens(amount, EDAI, {from : user1});
-    //     await dex.createLimitOrder(SIDE.SELL, EBAT, web3.utils.toWei('10'), 10, {from : user1});
+    it('should NOT create LIMIT BUY order if token is EDAI', async() => {
+        await expectRevert(
+            dex.createLimitOrder(SIDE.BUY, 
+                EDAI,
+                web3.utils.toWei('10'), 
+                30, 
+                {from : user1}),
+            'EDAI is not allowed to be traded'
+        );
+    });
 
-    //     const buyOrdersList = await dex.getOrders(SIDE.BUY, EBAT);
-    //     const sellOrdersList = await dex.getOrders(SIDE.SELL, EBAT);
-    //     assert(buyOrdersList.length === 1);
-    //     assert(buyOrdersList[0].trader === user1);
-    //     assert(buyOrdersList[0].ticker === web3.utils.padRight(EBAT, 64));
-    //     assert(buyOrdersList[0].price === '10');
-    //     assert(buyOrdersList[0].amount === web3.utils.toWei('10'));
-    //     assert(sellOrdersList.length === 0);
-    // });
+    it('should NOT create LIMIT SELL order if token balance is too low', async() => {
+        const amount = web3.utils.toWei('500');
 
-    // it('should NOT create a LIMIT order', async () => {
-    //     const amount = web3.utils.toWei('100');
+        await dex.depositTokens(amount, EBAT, {from : user1});
 
-    //     await dex.depositTokens(amount, EDAI, {from : user1});
-    //     await dex.
-    // });
+        await expectRevert(
+            dex.createLimitOrder(SIDE.SELL, EBAT, web3.utils.toWei('501'), 1, {from : user1}),
+            'Token balance is too low'
+        );
+    });
+
+    it('should NOT create LIMIT BUY order if EDAI balance is too low', async() => {
+        
+        const amount = web3.utils.toWei('500');
+
+        await dex.depositTokens(amount, EDAI, {from : user1});
+        await expectRevert(
+            dex.createLimitOrder(SIDE.BUY, EBAT, web3.utils.toWei('501'), 1, {from : user1}),
+            'EDAI balance is too low'
+        );
+    });
+
+    it('Should create a market order and match it with a limit order', async() => {
+        const amount = web3.utils.toWei('500');
+
+        await dex.depositTokens(amount, EDAI, {from : user1});
+        await dex.createLimitOrder(SIDE.BUY, EGNT, web3.utils.toWei('100'), 1, {from : user1});
+        await dex.depositTokens(amount, EGNT, {from : user2});
+        await dex.createMarketOrder(SIDE.SELL, EGNT, web3.utils.toWei('75'), {from : user2});
+
+        const balances = await Promise.all([
+            dex.traderBalances(user1, EGNT),
+            dex.traderBalances(user2, EGNT),
+            dex.traderBalances(user1, EDAI),
+            dex.traderBalances(user2, EDAI)
+        ])
+
+        const orders = await dex.getOrders(SIDE.BUY, EGNT);
+        assert(balances[0].toString() === web3.utils.toWei('75'));
+        assert(balances[1].toString() === web3.utils.toWei('425'));
+        assert(balances[2].toString() === web3.utils.toWei('425'));
+        assert(balances[3].toString() === web3.utils.toWei('75'));
+        assert(orders[0].filled === web3.utils.toWei('75'));
+    });
+
+    it('should NOT create MARKET order if the token does not exist', async() => {
+        await expectRevert(
+            dex.createMarketOrder(SIDE.BUY, 
+                web3.utils.fromAscii('DOES_NOT_EXIST'),
+                web3.utils.toWei('10'),
+                {from : user1}),
+            'Token must be tradable on EldoraDEX'
+        );
+    });
+
+    it('should NOT create MARKET order if token is EDAI', async() => {
+        await expectRevert(
+            dex.createMarketOrder(SIDE.BUY, 
+                EDAI,
+                web3.utils.toWei('10'),
+                {from : user1}),
+            'EDAI is not allowed to be traded'
+        );
+    });
+
+    it('should NOT create MARKET SELL order if token balance is too low', async() => {
+        const amount = web3.utils.toWei('500');
+
+        await dex.depositTokens(amount, EBAT, {from : user1});
+
+        await expectRevert(
+            dex.createMarketOrder(SIDE.SELL, EBAT, web3.utils.toWei('501'), {from : user1}),
+            'Token balance is too low'
+        );
+    });
+
+    it('should NOT create MARKET BUY order if EDAI balance is too low', async() => {
+        
+        const amount = web3.utils.toWei('500');
+
+        await dex.depositTokens(amount, EBAT, {from : user2});
+        await dex.createLimitOrder(SIDE.SELL, EBAT, web3.utils.toWei('100'), 1, {from : user2});
+
+        await expectRevert(
+            dex.createMarketOrder(SIDE.BUY, EBAT, web3.utils.toWei('100'), {from : user1}),
+            'EDAI balance is too low'
+        );
+    });
 });
