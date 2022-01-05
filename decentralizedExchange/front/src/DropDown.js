19595c5338129f4539211c58d17241dc18cfbdfb
ASAPSegfault <ASAP@Segfault.com>
2022-01-05 22:49:53 +0100

    Implementation of the DropDown component to switch between tokens on the exchange

---
+++ decentralizedExchange/front/src/DropDown.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+function DropDown({onSelect, activeItem, items}) {
+    const [dropDownVisible, setDropDownVisible] = useState(false);
+
+    const selectItem = (event, item) => {
+        event.preventDefault();
+        setDropDownVisible(!dropDownVisible);
+        onSelect(item);
+    }
+
+    return (<div className="dropdown ml-3">
+        <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            onClick={() => setDropDownVisible(!dropDownVisible)}>
+            {activeItem}
+        </button>
+        <div
+        className={`dropdown-menu ${dropDownVisible ? 'visible' : ''}`}>
+            {items && items.map((item, i) => {
+                <a className={`dropdown-item ${item.value === activeItem.value ? 'active' : null}`}
+                    href="#"
+                    onClick={event => selectItem(event, item.value)}
+                    key={i}>
+                        {item.label}
+                </a>
+            })}
+        </div>
+    </div>);
+}
+
+export default DropDown;
