70847f15e930c60ac5cb4bc133294017ef49d25b
ASAPSegfault <hugo.nakache@epitech.eu>
2022-03-06 16:18:31 +0100

    NFT_ERC1155 : Creates popup to visualise an NFT's details

--- NFT_ERC1155/NFT_Visualiser/fellowship_of_the_ring_visualiser/src/App.js
+++ NFT_ERC1155/NFT_Visualiser/fellowship_of_the_ring_visualiser/src/App.js
@@ -1,23 +1,153 @@
-import { NFTCard } from './components/NFTCard';
+import { useState } from 'react';
+import styled from 'styled-components';
+import { NFTCard, NftImage, NftPopupImage } from './components/NFTCard';
 
 function App() {
 
-  let dummynft = [ { name: "Aragorn", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150"},
-    { name: "Frodo", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150"},
-    { name: "Gandalf", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150"},
-    { name: "Boromir", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150"},
+  const [showPopup, setShowPopup] = useState(false);
+  const [chosenNft, setChosenNft] = useState();
+
+  let dummynft = [{ name: "Aragorn", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150" },
+  { name: "Frodo", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150" },
+  { name: "Gandalf", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150" },
+  { name: "Boromir", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150" },
+  { name: "Aragorn", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150" },
+  { name: "Frodo", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150" },
+  { name: "Gandalf", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150" },
+  { name: "Boromir", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150" },
   ]
+
+  function togglePopup(nftIndex) {
+    if (nftIndex >= 0) {
+      setChosenNft(dummynft[nftIndex]);
+    }
+    setShowPopup(!showPopup);
+  }
 
   return (
     <div className="App">
-      {
-        dummynft.map((nft, index) => 
-        <NFTCard nft={nft} key={index}>
+      <Container>
+        <Title> Fellowship Of The Ring Collection </Title>
+        <SubTitle> The most epic fellowship of all times </SubTitle>
+        <Grid>
+          {
+            dummynft.map((nft, index) =>
+              <NFTCard nft={nft} key={index} togglePopup={ () => togglePopup(index)}>
 
-        </NFTCard>)
+              </NFTCard>)
+          }
+        </Grid>
+      </Container>
+      {
+        showPopup && <NFTPopup nft={chosenNft} togglePopup={ () => togglePopup()}/>
       }
     </div>
   );
 }
+
+const NFTPopup = (props) => {
+  let nft = props.nft;
+  return (
+    <Popup>
+      <PopupContent>
+        <PopupGrid>
+          <NftPopupImage style={{ backgroundImage: `url(${nft && nft.image})`}}/>
+          <div>
+            <PopupTitle>
+              {nft.name}
+            </PopupTitle>
+            <PopupTextContainer>
+              { `You currently have ownership of ${ nft.copies } copies `}
+            </PopupTextContainer>
+            <PopupSubtitleSection>
+              Description
+            </PopupSubtitleSection>
+            <PopupTextContainer>
+              {nft.description}
+            </PopupTextContainer>
+            <PopupSubtitleSection>
+                Attributes
+            </PopupSubtitleSection>
+          </div>
+        </PopupGrid>
+        <CloseBtn onClick={ () => props.togglePopup()}>
+          &times;
+        </CloseBtn>
+      </PopupContent>
+    </Popup>
+  )
+}
+
+const Title = styled.h1`
+  margin: 0;
+  text-align: center;
+`
+const SubTitle = styled.h4`
+  margin-top: 0;
+  text-align: center;
+  color: gray;
+`
+
+const Container = styled.div`
+  width: 70%;
+  margin: auto;
+  margin-top: 200px;
+  max-width: 1200px;
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  row-gap: 40px;
+`
+
+const Popup = styled.div`
+  position: fixed;
+  display: flex;
+  align-items: center;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0,0,0,0.5);
+  overflow: auto;
+`
+
+const PopupContent = styled.div`
+  position: relative;
+  width: 900px;
+  margin: auto;
+  padding: 20px;
+  border-radius: 20px;
+  background-color: white;
+`
+
+const PopupTitle = styled.h1`
+  margin: 0;
+`
+
+const PopupTextContainer = styled.p`
+  margin: 0 0 15px 0;
+`
+
+const PopupSubtitleSection = styled.h3`
+  margin: 5px 0 5px 0;
+`
+const PopupGrid = styled.div`
+  display: inline-grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 40px;
+`
+
+const CloseBtn = styled.span`
+  position: absolute;
+  padding: 25px 25px 0 0;
+  right: 0;
+  top: 0;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
+`
 
 export default App;
--- NFT_ERC1155/NFT_Visualiser/fellowship_of_the_ring_visualiser/src/components/NFTCard.js
+++ NFT_ERC1155/NFT_Visualiser/fellowship_of_the_ring_visualiser/src/components/NFTCard.js
@@ -1,45 +1,45 @@
 import styled from 'styled-components';
 
 const NFTCard = (props) => {
-    let nft = props.nft;
-  
-    return (
-      <NftCard>
-        <NftImage style={{ backgroundImage: `url(${nft && nft.image})`}}/>
-        <div style={{margin: 5}}>
-         <NftCollectionText>
-           { nft && nft.symbol }
-         </NftCollectionText>
-         <NftName> 
-           { nft && nft.name}
-         </NftName>
-         <NftNumberOfCopies> 
-           { `x${ nft && nft.copies }`}
-         </NftNumberOfCopies>
-        </div>
-      </NftCard>
-    )
-  }
-  
-  const NftCollectionText = styled.div`
+  let nft = props.nft;
+
+  return (
+    <NftCard onClick={() => props.togglePopup() }>
+      <NftImage style={{ backgroundImage: `url(${nft && nft.image})` }} />
+      <div style={{ margin: 5 }}>
+        <NftCollectionText>
+          {nft && nft.symbol}
+        </NftCollectionText>
+        <NftName>
+          {nft && nft.name}
+        </NftName>
+        <NftNumberOfCopies>
+          {`x${nft && nft.copies}`}
+        </NftNumberOfCopies>
+      </div>
+    </NftCard>
+  )
+}
+
+const NftCollectionText = styled.div`
     font-size: 12px;
     color: grey;
   `
-  
-  const NftName = styled.div`
+
+const NftName = styled.div`
     font-size: 12px;
     font-weight: bold;
     display: inline;
   `
-  
-  const NftNumberOfCopies = styled.div`
+
+const NftNumberOfCopies = styled.div`
     font-size: 12px;
     font-weight: bold;
     display: inline;
     float: right;
   `
-  
-  const NftCard = styled.div`
+
+const NftCard = styled.div`
     width: 200px;
     height: 250px;
     margin: auto;
@@ -49,7 +49,7 @@ const NFTCard = (props) => {
     box-shadow: 6px 6px 12px #d9d9d9,
                 -6px -6px 12px #ffffff
   `
-  const NftImage = styled.div`
+const NftImage = styled.div`
     display: block;
     width: 200px;
     height: 200px;
@@ -59,4 +59,14 @@ const NFTCard = (props) => {
     margin: auto;
   `
 
-export { NFTCard, NftImage }
+  const NftPopupImage = styled.div`
+  display: block;
+  width: 400px;
+  height: 400px;
+  background-position: center center;
+  background-size: cover;
+  border-radius: 10px;
+  margin: auto;
+`
+
+export { NFTCard, NftImage, NftPopupImage}
