42be21656aa1ccd7f4ae75acc1b9f759eebf68ba
ASAPSegfault <hugo.nakache@epitech.eu>
2022-03-06 13:02:48 +0100

    NFT_ERC1155 : Creates basic NFT Card component that will hold the NFTs

--- NFT_ERC1155/NFT_Visualiser/fellowship_of_the_ring_visualiser/src/App.js
+++ NFT_ERC1155/NFT_Visualiser/fellowship_of_the_ring_visualiser/src/App.js
@@ -1,74 +1,23 @@
-import styled from 'styled-components';
+import { NFTCard } from './components/NFTCard';
 
 function App() {
 
-  let dummynft = { name: "Aragorn", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150"} 
+  let dummynft = [ { name: "Aragorn", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150"},
+    { name: "Frodo", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150"},
+    { name: "Gandalf", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150"},
+    { name: "Boromir", symbol: "FOTRC", copies: 10, image: "https://via.placeholder.com/150"},
+  ]
 
   return (
     <div className="App">
-      <NFTcard nft={dummynft}>
-      </NFTcard>
+      {
+        dummynft.map((nft, index) => 
+        <NFTCard nft={nft} key={index}>
+
+        </NFTCard>)
+      }
     </div>
   );
 }
-
-const NFTcard = (props) => {
-  let nft = props.nft;
-
-  return (
-    <NftCard>
-      <NftImage style={{ backgroundImage: `url(${nft && nft.image})`}}/>
-      <div style={{margin: 5}}>
-       <NftCollectionText>
-         { nft && nft.symbol }
-       </NftCollectionText>
-       <NftName> 
-         { nft && nft.name}
-       </NftName>
-       <NftNumberOfCopies> 
-         { `x${ nft && nft.copies }`}
-       </NftNumberOfCopies>
-      </div>
-    </NftCard>
-  )
-}
-
-const NftCollectionText = styled.div`
-  font-size: 12px;
-  color: grey;
-`
-
-const NftName = styled.div`
-  font-size: 12px;
-  font-weight: bold;
-  display: inline;
-`
-
-const NftNumberOfCopies = styled.div`
-  font-size: 12px;
-  font-weight: bold;
-  display: inline;
-  float: right;
-`
-
-const NftCard = styled.div`
-  width: 200px;
-  height: 250px;
-  margin: auto;
-  border-radius: 10px;
-  padding: 0px;
-  cursor: pointer;
-  box-shadow: 6px 6px 12px #d9d9d9,
-              -6px -6px 12px #ffffff
-`
-const NftImage = styled.div`
-  display: block;
-  width: 200px;
-  height: 200px;
-  background-position: center center;
-  background-size: cover;
-  border-radius: 10px;
-  margin: auto;
-`
 
 export default App;
---
+++ NFT_ERC1155/NFT_Visualiser/fellowship_of_the_ring_visualiser/src/components/NFTCard.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+const NFTCard = (props) => {
+    let nft = props.nft;
+  
+    return (
+      <NftCard>
+        <NftImage style={{ backgroundImage: `url(${nft && nft.image})`}}/>
+        <div style={{margin: 5}}>
+         <NftCollectionText>
+           { nft && nft.symbol }
+         </NftCollectionText>
+         <NftName> 
+           { nft && nft.name}
+         </NftName>
+         <NftNumberOfCopies> 
+           { `x${ nft && nft.copies }`}
+         </NftNumberOfCopies>
+        </div>
+      </NftCard>
+    )
+  }
+  
+  const NftCollectionText = styled.div`
+    font-size: 12px;
+    color: grey;
+  `
+  
+  const NftName = styled.div`
+    font-size: 12px;
+    font-weight: bold;
+    display: inline;
+  `
+  
+  const NftNumberOfCopies = styled.div`
+    font-size: 12px;
+    font-weight: bold;
+    display: inline;
+    float: right;
+  `
+  
+  const NftCard = styled.div`
+    width: 200px;
+    height: 250px;
+    margin: auto;
+    border-radius: 10px;
+    padding: 0px;
+    cursor: pointer;
+    box-shadow: 6px 6px 12px #d9d9d9,
+                -6px -6px 12px #ffffff
+  `
+  const NftImage = styled.div`
+    display: block;
+    width: 200px;
+    height: 200px;
+    background-position: center center;
+    background-size: cover;
+    border-radius: 10px;
+    margin: auto;
+  `
+
+export { NFTCard, NftImage }
